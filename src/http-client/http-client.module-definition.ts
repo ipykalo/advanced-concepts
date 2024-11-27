c2af310b17210c65a0703a95a823076c0c1ab42d
Ivan <ipykaloi@gmail.com>
2024-11-27 14:23:39 +0200

    Building Configurable Modules

---
+++ src/http-client/http-client.module-definition.ts
@@ -0,0 +1,24 @@
+import { ConfigurableModuleBuilder } from '@nestjs/common';
+
+// Building Configurable Modules
+
+export const {
+  ConfigurableModuleClass,
+  MODULE_OPTIONS_TOKEN: HTTP_MODULE_OPTIONS,
+  OPTIONS_TYPE,
+  ASYNC_OPTIONS_TYPE,
+} = new ConfigurableModuleBuilder<{
+  baseUrl?: string;
+}>({ alwaysTransient: true })
+  // .setClassMethodName('forRoot')
+  // .setFactoryMethodName('resolve')
+  .setExtras<{ isGlobal?: boolean }>(
+    {
+      isGlobal: true,
+    },
+    (definition, extras) => ({
+      ...definition,
+      global: extras.isGlobal,
+    }),
+  )
+  .build();
